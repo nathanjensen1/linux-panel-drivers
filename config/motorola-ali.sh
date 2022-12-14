cc39228b3e627537493bf51fd699df1611140745
Nathan Jensen <nathanjensen1@outlook.com>
2022-12-14 11:26:36 -0700

    Add motorola-ali panels

---
+++ config/motorola-ali.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[tianma_565_1080p_vid_v0]="tianma,tl057fvxp01"
+	[boe_565_1080p_vid_v0]="boe,bs057y8m-n00-680g"
+	[auo_565_1080p_vid_v0]="auo,h565dan01"
+)
